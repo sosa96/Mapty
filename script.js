cb796ab1defa2fa5a8633f932107f81fedc594b9
Zdravko Sosa <zdravko.sosa@gmail.com>
2021-04-07 18:12:29 +0200

    Implemented moving marker on click

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat,lng]
@@ -25,6 +26,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -66,6 +71,7 @@ class Cycling extends Workout {
 class App {
   // Private instance properties
   #map;
+  #mapZoomLevel = 13;
   #mapEvent;
   #workouts = [];
 
@@ -77,6 +83,8 @@ class App {
     // Bind because event listener points this keyword to form element not app object
 
     inputType.addEventListener('change', this._toggleElevationField);
+
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -97,7 +105,7 @@ class App {
 
       const coords = [latitude, longitude];
 
-      this.#map = L.map('map').setView(coords, 13);
+      this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
@@ -159,9 +167,6 @@ class App {
 
       workout = new Running([lat, lng], distance, duration, cadence);
     }
-
-    // Add new object to workout array
-    this.#workouts.push(workout);
 
     // If workout cycling, create cycling object
     if (type === 'cycling') {
@@ -257,6 +262,26 @@ class App {
     `;
 
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+
+    // Using the public interface
+    workout.click();
   }
 }
 
